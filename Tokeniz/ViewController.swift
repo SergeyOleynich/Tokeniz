fc0534be4d18bac8111843be26f048ab8e4c39a1
Serhii Oleinich <sergeyoleynich@gmail.com>
2024-01-11 17:35:34 +0200

    Create empty project. Add gitignore.

---
+++ Tokeniz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tokeniz
+//
+//  Created by Serhii Oleinich on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
